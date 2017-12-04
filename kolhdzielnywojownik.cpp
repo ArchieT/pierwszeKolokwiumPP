6b48c2219dd606494a4f3529a0315d1b9014b365
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2017-12-04 07:00:38 +0100

    kolH: abs

--- kolhdzielnywojownik.cpp
+++ kolhdzielnywojownik.cpp
@@ -3,13 +3,17 @@
 
 using namespace std;
 
+long long int abs(long long int x) {
+    return x<0 ? -x : x;
+}
+
 int main() {
     while(true) {
         long long int left;
         long long int right;
         cin >> left;
         cin >> right;
-        cout << right-left << endl;
+        cout << abs(right-left) << endl;
         if(getchar()==EOF) return 0;
     }
 }
